ec75d8d0a2fa9dce7148d57d2b719cde0d7186ac
KamenIaNeDam <nspanda101@gmail.com>
2025-01-23 22:57:21 +0700

    Init

---
+++ proj/settings/prod.py
@@ -0,0 +1,16 @@
+import os
+
+from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "planner"),
+        "USER": os.environ.get("DB_USERNAME", "custom"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+    }
+}
